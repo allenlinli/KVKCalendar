cb0b1aba08d90b8fb5d9ab752ef6d17e8e7e837c
allenlinli-case <allen.lin@cave-i.com>
2021-04-13 14:31:06 +0800

    feat: make today the default date for CalendarView

--- Example/KVKCalendar/ViewController.swift
+++ Example/KVKCalendar/ViewController.swift
@@ -14,9 +14,10 @@ final class ViewController: UIViewController {
     private var events = [Event]()
     
     private var selectDate: Date = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "dd.MM.yyyy"
-        return formatter.date(from: "14.12.2020") ?? Date()
+//        let formatter = DateFormatter()
+//        formatter.dateFormat = "dd.MM.yyyy"
+//        return formatter.date(from: "14.04.2021") ?? Date()
+        return Date()
     }()
     
     private lazy var todayButton: UIBarButtonItem = {
@@ -235,7 +236,6 @@ extension ViewController: CalendarDataSource {
             return cell
         case .day, .week, .month:
             guard dateParameter.date?.day == Date().day else { return nil }
-            
             let cell = (view as? UICollectionView)?.dequeueCell(indexPath: indexPath) { (cell: CustomDayCell) in
                 cell.imageView.image = UIImage(named: "ic_stub")
             }
